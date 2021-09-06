d61f4cf8d61a7727356f78cbc4b61cf9ea9be9bd
ppzeff <ppzeff@gmail.com>
2021-09-06 11:54:25 +0300

    first commit

---
+++ src/main/java/com/dehssisfs/redirect/controllers/MainController.java
@@ -0,0 +1,27 @@
+package com.dehssisfs.redirect.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Главная страница");
+        return "home";
+    }
+
+    @GetMapping("/workpermit/loto")
+    public String wpl(Model model) {
+        return "redirect:http://46.249.7.119:8080/";
+    }
+    @PostMapping("/workpermit/loto/regLOTO")
+    public String wplr(Model model) {
+        return "redirect:http://46.249.7.119:8080/regLOTO";
+    }
+
+}
